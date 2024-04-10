ae03d8911ccb36f9c062dd8656c7bf771013af83
Oksana57 <114564080+Oksana57@users.noreply.github.com>
2024-04-10 09:00:53 +0300

    Create dz3_1_tr.py
    подготовка ДЗ

---
+++ dz3_1_tr.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+lst_el = [1, 34, 2, 1, 35, 1, 2, 34, 3, 5]
+dct_dubl = Counter(lst_el)
+#print(dct_dubl)
+
+lst_dubl = []
+for k, v in dct_dubl.items():
+    if v > 1:
+        lst_dubl.append(k)
+    else:
+        continue
+print(list(set(lst_dubl)))
